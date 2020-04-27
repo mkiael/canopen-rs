3bbd34d8be15d4136237b6b60519c53d5c9506c3
Mikael Larsson <mikael.larsson@epiroc.com>
2020-04-27 22:41:27 +0200

    Add support for subscribing on object in dictionary

--- src/od.rs
+++ src/od.rs
@@ -1,8 +1,10 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 use std::string::String;
 
 #[derive(Hash, Eq, PartialEq)]
-pub struct ObjectKey {
+struct ObjectKey {
     index: u16,
     sub_index: u8,
 }
@@ -18,31 +20,68 @@ pub enum ObjectValue {
     Real32(f32),
     VisibleString(String),
     OctetString(String),
-    UnicodeString(String)
+    UnicodeString(String),
 }
 
-pub struct ObjectDictionary {
-    dict: HashMap<ObjectKey, ObjectValue>
+pub trait ObjectSubscriber {
+    fn object_updated(&mut self, index: &u16, sub_index: &u8, value: &ObjectValue);
 }
 
-impl ObjectDictionary {
-    pub fn new() -> ObjectDictionary {
-        ObjectDictionary{ dict: HashMap::new() }
-    }
+struct Object {
+    index: u16,
+    sub_index: u8,
+    value: ObjectValue,
+    subscribers: Vec<Rc<RefCell<dyn ObjectSubscriber>>>,
+}
 
-    pub fn add(&mut self, index: u16, sub_index: u8, value: ObjectValue) {
-        self.dict.insert(ObjectKey{index, sub_index}, value);
-    }
-
-    pub fn write(&mut self, index: u16, sub_index: u8, value: ObjectValue) {
-        if let Some(x) = self.dict.get_mut(&ObjectKey { index, sub_index }) {
-            if std::mem::discriminant(x) == std::mem::discriminant(&value) {
-                *x = value;
+impl Object {
+    pub fn write(&mut self, value: ObjectValue) {
+        if std::mem::discriminant(&self.value) == std::mem::discriminant(&value) {
+            self.value = value;
+            for subscriber in self.subscribers.iter_mut() {
+                subscriber.borrow_mut().object_updated(&self.index, &self.sub_index, &self.value);
             }
         }
     }
 
+    pub fn read(&self) -> &ObjectValue {
+        return &self.value;
+    }
+
+    pub fn subscribe(&mut self, subscriber: Rc<RefCell<dyn ObjectSubscriber>>) {
+        self.subscribers.push(subscriber);
+    }
+}
+
+pub struct ObjectDictionary {
+    dict: HashMap<ObjectKey, Object>,
+}
+
+impl ObjectDictionary {
+    pub fn new() -> ObjectDictionary {
+        ObjectDictionary { dict: HashMap::new() }
+    }
+
+    pub fn add(&mut self, index: u16, sub_index: u8, value: ObjectValue) {
+        self.dict.insert(ObjectKey { index, sub_index }, Object { index, sub_index, value, subscribers: Vec::new()});
+    }
+
+    pub fn write(&mut self, index: u16, sub_index: u8, value: ObjectValue) {
+        if let Some(obj) = self.dict.get_mut(&ObjectKey { index, sub_index }) {
+            obj.write(value);
+        }
+    }
+
     pub fn read(&self, index: u16, sub_index: u8) -> Option<&ObjectValue> {
-        return self.dict.get(&ObjectKey{index, sub_index });
+        match self.dict.get(&ObjectKey { index, sub_index }) {
+            Some(obj) => Some(obj.read()),
+            None => None
+        }
+    }
+
+    pub fn subscribe(&mut self, index: u16, sub_index: u8, subscriber: Rc<RefCell<dyn ObjectSubscriber>>) {
+        if let Some(obj) = self.dict.get_mut(&ObjectKey { index, sub_index }) {
+            obj.subscribe(subscriber);
+        }
     }
 }
