19b3e545fa04ede54ca0841949f0ff90d5671a89
Mikael Larsson <mikael.larsson@epiroc.com>
2020-05-03 21:47:47 +0200

    Refactor node control service

--- src/service/node_control.rs
+++ src/service/node_control.rs
@@ -11,47 +11,35 @@ pub enum NodeCommand {
     None,
 }
 
-pub struct NodeControl {
-    node_id: u8,
+pub fn handle_nmt_message(node_id: u8, can_message: CanMessage) -> NodeCommand {
+    if is_message_valid(node_id, &can_message) {
+        match can_message.data()[0] {
+            0x1 => NodeCommand::StartNode,
+            0x2 => NodeCommand::StopNode,
+            0x80 => NodeCommand::EnterPreOperational,
+            0x81 => NodeCommand::ResetNode,
+            0x82 => NodeCommand::ResetCommunication,
+            _ => NodeCommand::None,
+        }
+    } else {
+        NodeCommand::None
+    }
 }
 
-impl NodeControl {
-    pub fn new(node_id: u8) -> NodeControl {
-        NodeControl { node_id }
-    }
-
-    pub fn process(&self, can_message: CanMessage) -> NodeCommand {
-        if self.is_message_valid(&can_message) {
-            match can_message.data()[0] {
-                0x1 => NodeCommand::StartNode,
-                0x2 => NodeCommand::StopNode,
-                0x80 => NodeCommand::EnterPreOperational,
-                0x81 => NodeCommand::ResetNode,
-                0x82 => NodeCommand::ResetCommunication,
-                _ => NodeCommand::None,
-            }
-        } else {
-            NodeCommand::None
-        }
-    }
-
-    fn is_message_valid(&self, can_message: &CanMessage) -> bool {
-        (can_message.cob() == Cob::Nmt)
-            && (can_message.data_length() == 2)
-            && (can_message.data()[1] == self.node_id)
-    }
+fn is_message_valid(node_id: u8, can_message: &CanMessage) -> bool {
+    (can_message.cob() == Cob::Nmt)
+        && (can_message.data_length() == 2)
+        && (can_message.data()[1] == node_id)
 }
 
 #[cfg(test)]
 mod tests {
     use crate::cob::Cob;
     use crate::message::CanMessage;
-    use crate::service::node_control::NodeCommand;
-    use crate::service::node_control::NodeControl;
+    use crate::service::node_control::*;
 
     fn test_node_command(node_cmd: NodeCommand, cs: u8) {
-        let node_control = NodeControl::new(0x4);
-        let cmd = node_control.process(CanMessage::from_cob(Cob::Nmt, vec![cs, 0x4]));
+        let cmd = handle_nmt_message(0x4, CanMessage::from_cob(Cob::Nmt, vec![cs, 0x4]));
         assert_eq!(cmd, node_cmd);
     }
 
@@ -82,29 +70,25 @@ mod tests {
 
     #[test]
     fn test_invalid_message_not_nmt() {
-        let node_control = NodeControl::new(0x4);
-        let cmd = node_control.process(CanMessage::from_cob(Cob::Sync, vec![]));
+        let cmd = handle_nmt_message(0x4, CanMessage::from_cob(Cob::Sync, vec![]));
         assert_eq!(cmd, NodeCommand::None);
     }
 
     #[test]
     fn test_invalid_message_not_correct_data_length() {
-        let node_control = NodeControl::new(0x4);
-        let cmd = node_control.process(CanMessage::from_cob(Cob::Nmt, vec![]));
+        let cmd = handle_nmt_message(0x4, CanMessage::from_cob(Cob::Nmt, vec![]));
         assert_eq!(cmd, NodeCommand::None);
     }
 
     #[test]
     fn test_invalid_message_not_same_node_id() {
-        let node_control = NodeControl::new(0x4);
-        let cmd = node_control.process(CanMessage::from_cob(Cob::Nmt, vec![0x1, 0x5]));
+        let cmd = handle_nmt_message(0x4, CanMessage::from_cob(Cob::Nmt, vec![0x1, 0x5]));
         assert_eq!(cmd, NodeCommand::None);
     }
 
     #[test]
     fn test_invalid_message_unknown_command_specifier() {
-        let node_control = NodeControl::new(0x4);
-        let cmd = node_control.process(CanMessage::from_cob(Cob::Nmt, vec![0x63, 0x4]));
+        let cmd = handle_nmt_message(0x4, CanMessage::from_cob(Cob::Nmt, vec![0x63, 0x4]));
         assert_eq!(cmd, NodeCommand::None);
     }
 }
